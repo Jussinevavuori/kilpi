6a7d5367e42d6c1b1119c6a00831fde1b97051e6
Jussi Nevavuori <jussi@nevavuori.fi>
2025-01-08 16:42:55 +1000

    feat: new placeholder test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,7 @@
-import { describe, it, expect } from 'bun:test'
-import { one, two } from '../src'
+import { describe, expect, it } from "bun:test";
 
-describe('should', () => {
-  it('export 1', () => {
-    expect(one).toBe(1)
-  })
-
-  it('export 2', () => {
-    expect(two).toBe(2)
-  })
-})
+describe("should", () => {
+  it("skip tests until I write them", () => {
+    expect(1).toBe(1);
+  });
+});
