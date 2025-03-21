a6051416e50075789f605186fd7ffcb82a0c5e5c
Jussi Nevavuori <jussi@nevavuori.fi>
2025-03-21 12:14:07 +1000

    docs: add posthog

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,6 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import starlightThemeBlack from "starlight-theme-black";
 
+const POSTHOG_SCRIPT_TAG_CONTENT = `
+!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+posthog.init('phc_dW98JhlU81kc2w4uzWSSS2OGR12Jf0HLVmVgj2aEbH6', { api_host: 'https://eu.i.posthog.com', person_profiles: 'always' })
+`;
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kilpi.vercel.app",
@@ -24,7 +29,9 @@ export default defineConfig({
       },
       customCss: ["./src/styles/docs.css"],
 
-      // Configure sidebar
+      /**
+       * Configure sidebar
+       */
       sidebar: [
         {
           label: "Home",
@@ -57,6 +64,18 @@ export default defineConfig({
       ],
 
       /**
+       * Custom head
+       */
+      head: [
+        // Posthog analytics
+        {
+          tag: "script",
+          attrs: { id: "posthog" },
+          content: POSTHOG_SCRIPT_TAG_CONTENT,
+        },
+      ],
+
+      /**
        * Custom theme
        */
       plugins: [
@@ -68,10 +87,11 @@ export default defineConfig({
             },
           ],
           footerText: [
-            "Designed & created with ❤️ by [Jussi Nevavuori](https://www.jussinevavuori.com) in Brisbane & Helsinki. ",
-            "Source code available on [GitHub](https://www.github.com/jussinevavuori/kilpi).",
-            "All code is licensed under the [MIT License](https://opensource.org/licenses/MIT).",
-          ].join(" "),
+            "👨🏻‍💻 Designed & created with ❤️ by [Jussi Nevavuori](https://www.jussinevavuori.com) in Brisbane & Helsinki",
+            "👀 Source code available on [GitHub](https://www.github.com/jussinevavuori/kilpi)",
+            "📄 All code is licensed under the [MIT License](https://opensource.org/licenses/MIT)",
+            "⚡ Powered by [Astro](https://astro.build), [Starlight](https://starlight.astro.build/) & [Vercel](https://vercel.com)",
+          ].join(" // "),
         }),
       ],
     }),
