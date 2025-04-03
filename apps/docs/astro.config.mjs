6508229759e659f28c296022082314bc0a2ba393
Jussi Nevavuori <jussi@nevavuori.fi>
2025-04-03 17:02:37 +1000

    fix: docs build

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -32,6 +32,8 @@ export default defineConfig({
       // Styling with TailwindCSS
       tailwindcss(),
     ],
+    // External dependencies (fixing errors)
+    build: { rollupOptions: { external: ["tslib"] } },
   },
 
   // Configure redirects
