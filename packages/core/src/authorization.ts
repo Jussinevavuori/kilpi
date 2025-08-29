d364f54db3aecd6ce56fd27807502ef03d370104
Jussi Nevavuori <jussi@nevavuori.fi>
2025-08-29 08:42:20 +0300

    feat: enable adding optional metadata to denials

--- packages/core/src/authorization.ts
+++ packages/core/src/authorization.ts
@@ -24,6 +24,11 @@ export type AuthorizationDenied = {
    * errors differently.
    */
   type?: string;
+
+  /**
+   * Metadata that can optionally be added to the denial.
+   */
+  metadata?: unknown;
 };
 
 /**
@@ -43,10 +48,11 @@ export function grant<TSubject>(subject: TSubject): AuthorizationGranted<TSubjec
  * Utility to create an AuthorizationDenied object with a message.
  */
 export function deny(
-  input: string | Pick<AuthorizationDenied, "message" | "type"> = "Unauthorized",
+  input: string | Pick<AuthorizationDenied, "message" | "type" | "metadata"> = "Unauthorized",
 ): AuthorizationDenied {
   const message = typeof input === "string" ? input : input.message;
   const type = typeof input === "string" ? undefined : input.type;
+  const metadata = typeof input === "string" ? undefined : input.metadata;
 
-  return { granted: false, message, type };
+  return { granted: false, message, type, metadata };
 }
