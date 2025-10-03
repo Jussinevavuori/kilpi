c8e65ab4bf2aed5709921f73d1a292a8dc744a08
Jussi Nevavuori <jussi@nevavuori.fi>
2025-10-03 15:53:48 +0300

    feat: rewrite quickstart, authorizing, policies, subject

--- apps/docs/src/consts.ts
+++ apps/docs/src/consts.ts
@@ -8,16 +8,16 @@ export const SIDEBAR_FOLDER_CONFIG: Array<{
     name: "Getting Started",
   },
   {
-    id: "installation",
-    name: "Installation",
-  },
-  {
     id: "concepts",
     name: "Concepts",
   },
   {
     id: "plugins",
     name: "Plugins",
+  },
+  {
+    id: "installation",
+    name: "Installation",
   },
   {
     id: "advanced",
