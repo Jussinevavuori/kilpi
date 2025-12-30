eecf9574c257c4dea2bd42ff8bc06ea90c4c3e98
Jussi Nevavuori <jussi@nevavuori.fi>
2025-12-30 14:50:48 +0200

    fix: change internal implementation of kilpi query to allow for use with .d.ts files

---
+++ .changeset/easy-phones-press.md
@@ -0,0 +1,5 @@
+---
+"@kilpi/core": patch
+---
+
+change internal implementation of kilpi query to allow for usage with declaration files
--- packages/core/src/KilpiQuery.ts
+++ packages/core/src/KilpiQuery.ts
@@ -20,87 +20,73 @@ export type KilpiQueryAuthorizeFn<
 /**
  * Create protected queries.
  */
-export class KilpiQuery<
+export type KilpiQuery<
   TCore extends KilpiCore<any, any>,
   TInput extends any[],
   TRawOutput,
   TRedactedOutput = TRawOutput,
-> {
-  /**
-   * Reference to core instance.
-   */
-  private core: TCore;
-
-  /**
-   * Reference to internal query function.
-   */
-  private query: (...args: TInput) => TRawOutput;
-
+> = {
   /**
    * Type inference utilities. Do not use at runtime.
    */
-  public $$infer: {
+  $$infer: {
     core: TCore;
     input: TInput;
     rawOutput: TRawOutput;
     redactedOutput: TRedactedOutput;
-  } = {} as any;
-
-  /**
-   * Authorization function which is called after the query is executed to
-   * authorize or redact the output.
-   */
-  private authorize: KilpiQueryAuthorizeFn<
-    TInput,
-    TRawOutput,
-    TRedactedOutput,
-    TCore["$$infer"]["getSubject"]
-  >;
-
-  /**
-   * Constructor which assigns all parameters.
-   */
-  constructor(
-    core: TCore,
-    query: (...args: TInput) => TRawOutput,
-    options: {
-      authorize?: KilpiQueryAuthorizeFn<
-        TInput,
-        TRawOutput,
-        TRedactedOutput,
-        TCore["$$infer"]["getSubject"]
-      >;
-    } = {},
-  ) {
-    this.core = core;
-    this.query = query;
-
-    // Assign authorize output fn or use no-op authorize by default
-    this.authorize = options?.authorize ?? ((args) => args.output as unknown as TRedactedOutput);
-  }
-
-  // ===============================================================================================
-  // PUBLIC QUERY API
-  // ===============================================================================================
+  };
 
   /**
    * Unsafe access. No authorization or redaction.
    */
-  public async unauthorized(...args: TInput): Promise<Awaited<TRawOutput>> {
-    return await this.query(...args);
-  }
+  unauthorized: (...args: TInput) => Promise<Awaited<TRawOutput>>;
 
   /**
    * Safe access. Runs authorization and redaction.
    */
-  public async authorized(...input: TInput): Promise<Awaited<TRedactedOutput>> {
-    // Access subject
-    const subject = await KilpiCore.expose(this.core).getSubject();
+  authorized: (...input: TInput) => Promise<Awaited<TRedactedOutput>>;
+};
 
-    // Run query
-    const output = await this.query(...input);
+/**
+ * Create protected queries without classes.
+ */
+export const createKilpiQuery = <
+  TCore extends KilpiCore<any, any>,
+  TInput extends any[],
+  TRawOutput,
+  TRedactedOutput = TRawOutput,
+>(
+  core: TCore,
+  query: (...args: TInput) => TRawOutput,
+  options: {
+    authorize?: KilpiQueryAuthorizeFn<
+      TInput,
+      TRawOutput,
+      TRedactedOutput,
+      TCore["$$infer"]["getSubject"]
+    >;
+  } = {},
+): KilpiQuery<TCore, TInput, TRawOutput, TRedactedOutput> => {
+  // Assign authorize output fn or use no-op authorize by default
+  const authorize =
+    options?.authorize ?? ((args) => args.output as unknown as TRedactedOutput);
 
-    // Run through authorize to authorize or redact data
-    return await this.authorize({ input, output, subject });
-  }
-}
+  return {
+    $$infer: {} as any,
+
+    async unauthorized(...args: TInput): Promise<Awaited<TRawOutput>> {
+      return await query(...args);
+    },
+
+    async authorized(...input: TInput): Promise<Awaited<TRedactedOutput>> {
+      // Access subject
+      const subject = await KilpiCore.expose(core).getSubject();
+
+      // Run query
+      const output = await query(...input);
+
+      // Run through authorize to authorize or redact data
+      return await authorize({ input, output, subject });
+    },
+  };
+};
